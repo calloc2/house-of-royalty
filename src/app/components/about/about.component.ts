6d4171fdb17d076fc791969c9ac2d826e8439774
Yuri <119751708+comatic0@users.noreply.github.com>
2025-05-23 17:57:37 -0300

    testing new materials

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatCard } from '@angular/material/card';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [
+    MatCard
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
