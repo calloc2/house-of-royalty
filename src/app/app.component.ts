a31e7f12620498a740dbdc5580dc16006647d6e0
Yuri <119751708+comatic0@users.noreply.github.com>
2025-05-23 17:10:12 -0300

    creates landing page essential components and stylizes them

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsSliderComponent } from './components/projects-slider/projects-slider.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [
+    NavbarComponent,
+    HeroComponent,
+    AboutComponent,
+    ProjectsSliderComponent,
+    ContactComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/projects-slider/projects-slider.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projects-slider',
+  imports: [],
+  templateUrl: './projects-slider.component.html',
+  styleUrl: './projects-slider.component.scss'
+})
+export class ProjectsSliderComponent {
+
+}
