da4dc42c99702a96364555bbeb2babf3b43d3990
Yuri <119751708+comatic0@users.noreply.github.com>
2025-05-26 10:47:13 -0300

    adds favicon, navbar and sidebar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-navbar',
-  imports: [MatToolbarModule, MatButtonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatListModule,
+    MatSidenavModule,
+    MatIcon,
+    MatSidenav
+  ]
 })
 export class NavbarComponent {
-
+  @ViewChild('sidenav') sidenav!: MatSidenav;
 }
