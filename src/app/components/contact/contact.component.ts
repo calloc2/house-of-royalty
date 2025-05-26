626e5fed89f034814de7a82594fc939adc28da60
Yuri <119751708+comatic0@users.noreply.github.com>
2025-05-26 10:10:05 -0300

    adds social buttons with animations

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { MatCard } from '@angular/material/card';
+import { MatDivider } from '@angular/material/divider';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [MatCard,
+    MatIcon,
+    MatDivider,
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
