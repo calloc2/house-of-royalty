9d4a9cd2e4c02f2b28a394580cbffd0e7976abc9
calloc2 <119751708+calloc2@users.noreply.github.com>
2025-06-04 02:10:15 +0000

    Deploying to gh-pages from @ calloc2/house-of-royalty@fcadbf898f23986fdfc6400b0823cf40d0b037eb 🚀

--- server/angular-app-engine-manifest.mjs
+++ server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: 'https://calloc2.github.io/house-of-royalty',
+  basePath: '/house-of-royalty',
   supportedLocales: {
   "en-US": ""
 },
--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -2,13 +2,13 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: 'https://calloc2.github.io/house-of-royalty/',
+  baseHref: '/house-of-royalty/',
   locale: undefined,
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 25045, hash: 'b17957b0f224e826d983a2ce1cd8e5b5199923feee48436d067e8753aa419550', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 18591, hash: '256f342c190c7591d3696f577dc6fe473c9105100fb5aa7e2a630958a9b3fce8', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 25020, hash: '408475edb42c753a5969cb107b5326fcd2ae4b7715951504ad8565ef54668392', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 18566, hash: '52005ed9952cb15e0d0c3cd4ccc1374e7faa119f2403a571e0c38024bdaa45ba', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-IXNJZP7C.css': {size: 7007, hash: 'zw5Ogdww1Mw', text: () => import('./assets-chunks/styles-IXNJZP7C_css.mjs').then(m => m.default)}
   },
 };
